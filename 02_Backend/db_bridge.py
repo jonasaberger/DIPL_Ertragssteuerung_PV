89600676721779a5702d89ca1d8e0de5626d79b4
Tim Hechenberger <timhec44@gmail.com>
2025-10-03 16:39:09 +0200

    Refactor PV data handling to use a dictionary for improved flexibility and add additional fields for energy metrics

--- 02_Backend/db_bridge.py
+++ 02_Backend/db_bridge.py
@@ -30,19 +30,23 @@ class DB_Bridge:
             print(f"Connection error: {e}" )
 
     # Write PV data into InfluxDB
-    def write_pv_data(self, pv, load, grid, battery, soc):
+    def write_pv_data(self, data):
         try:
             point = (
-                # Point is the name of Table
-                Point("pv_measurements")  
-                .field("pv_power", float(pv))
-                .field("load_power", float(load))
-                .field("grid_power", float(grid))
-                .field("battery_power", float(battery))
-                .field("soc", float(soc))
+                Point("pv_measurements")
+                .field("pv_power", float(data.get("pv_power", 0)))
+                .field("load_power", float(data.get("load_power", 0)))
+                .field("grid_power", float(data.get("grid_power", 0)))
+                .field("battery_power", float(data.get("battery_power", 0)))
+                .field("soc", float(data.get("soc", 0)))
+                .field("e_day", float(data.get("e_day", 0)))
+                .field("e_year", float(data.get("e_year", 0)))
+                .field("e_total", float(data.get("e_total", 0)))
+                .field("rel_autonomy", float(data.get("rel_autonomy", 0)))
+                .field("rel_selfconsumption", float(data.get("rel_selfconsumption", 0)))
             )
             self.write_api.write(bucket=self.bucket, org=self.org, record=point)
             print("Data written to InfluxDB")
         except Exception as e:
-            print(f"Data has not been written to InfluxDb: {e}")
+            print(f"Data has not been written to InfluxDB: {e}")
   
