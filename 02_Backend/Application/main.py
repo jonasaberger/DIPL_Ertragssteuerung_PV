708a46eca659a9d599433b8ebc35062c8b2dde68
Tim Hechenberger <timhec44@gmail.com>
2025-10-16 09:50:33 +0200

    Adding a Backend-Application, added new implementations, update requirements and swagger

---
+++ 02_Backend/Application/main.py
@@ -0,0 +1,15 @@
+import service_manager as sm
+import db_bridge as ddb
+
+# This is the main entry point for the DIPL_Ertragssteuerung_PV Backend application.
+# Swagger UI is served at host_ip:server_port/swagger
+# By default that would be http://localhost:5050/swagger
+
+def main():
+    print("Starting PV Backend Service...")
+    service_manager = sm.ServiceManager()
+    dbbridge = ddb.DB_Bridge()
+    
+    dbbridge.check_connection()
+    service_manager.start_server()
+main()
