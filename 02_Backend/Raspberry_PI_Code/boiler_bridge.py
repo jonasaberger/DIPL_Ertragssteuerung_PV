016a9d48e24b129427004d3b51ff024113dd2de9
Tim Hechenberger <timhec44@gmail.com>
2025-11-27 11:24:01 +0100

    Add Boiler_Bridge class for reading boiler temperature data

---
+++ 02_Backend/Raspberry_PI_Code/boiler_bridge.py
@@ -0,0 +1,48 @@
+# boiler_bridge.py
+import os
+import glob
+import time
+import platform
+
+#Initialise temperatur sensor if not done by os yet
+if platform.system() == "Linux":
+    os.system('modprobe w1-gpio')
+    os.system('modprobe w1-therm')
+
+
+class Boiler_Bridge:
+    def __init__(self):
+        #Define basedir + folder of temperator sensor(s); Sensor connected to GPIO17 (defined in /boot/firmware/config.txt)
+        if platform.system() != "Linux":
+            # no 1-wire sensor on non-Linux systems
+            self.device_file = None
+            return
+
+        base_dir = '/sys/bus/w1/devices/'
+        device_folders = glob.glob(base_dir + '28*')
+        if not device_folders:
+            raise FileNotFoundError("No DS18B20 temperature sensor found under /sys/bus/w1/devices/")
+        self.device_file = device_folders[0] + '/w1_slave'
+
+    def read_temp_raw(self):            #read temperatur sensor file, raw, no formatting
+        if not self.device_file:
+            return None
+        with open(self.device_file, 'r') as f:
+            lines = f.readlines()
+        return lines
+
+    def read_temp(self):                #format values read in temperatur sensor file
+        if not self.device_file:
+            return None
+        lines = self.read_temp_raw()
+        while lines[0].strip()[-3:] != 'YES':       #prepared for more than one temperature sensor, therefore array
+            time.sleep(0.2)
+            lines = self.read_temp_raw()
+        equals_pos = lines[1].find('t=')            #filter position t= as after that the next 5 values are the temperatur *1000
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos+2:]
+            temp_c = float(temp_string) / 1000.0    #value found / 1000 = temeratur in °C
+            temp_c = int(temp_c)                    #remove comma values
+            #temp_f = temp_c * 9.0 / 5.0 + 32.0     #temperatur in Fahrenheit, no use for me
+            return temp_c
+        return None
